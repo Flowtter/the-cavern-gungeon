85abe3a0c6760934732052c1a18181cefe2bb690
Naexys <34689945+Naexys@users.noreply.github.com>
2019-09-24 11:58:49 +0200

    gungeon

---
+++ Assets/Scripts/Player/DashTime.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DashTime : MonoBehaviour
+{
+    public Image Dash;
+    public float x;
+    public RectTransform rt;
+    void Start()
+    {
+        Dash = this.GetComponent<Image>();
+        RectTransform rt = this.GetComponent<RectTransform>();
+    }
+
+    void Update()
+    {
+
+        x = (GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerDeplacement>().ActualTime / GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerDeplacement>().Cooldown) * 0.5f;
+        x = 0.5f - x;
+        rt.sizeDelta = new Vector2(x, 0.05f);
+    }
+}
+
